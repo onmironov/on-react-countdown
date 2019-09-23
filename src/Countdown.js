d194025ec891201d26f822c883d6f579cfd21c37
onmironov <mironov_on@pik.ru>
2019-09-23 14:03:18 +0300

    init

---
+++ src/Countdown.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Countdown = () => <span>Countdown</span>;
+
+export default Countdown;
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/Countdown.js',
+  output: {
+    path: path.resolve('lib'),
+    filename: 'Countdown.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+};

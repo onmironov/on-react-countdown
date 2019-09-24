3c75a3fb0e4ee5daacf0be8ea7c53692f6e5cc5e
onmironov <mironov_on@pik.ru>
2019-09-24 11:37:38 +0300

    add proptypes, end of words, lang

--- src/Countdown.js
+++ src/Countdown.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { time, getEndOfWord } from './helpers';
 
 class Countdown extends Component {
-  CONSTANTS = {
-    days   : 86400,
-    hours  : 3600,
-    minutes: 60,
+  static propTypes = {
+    /** Time of end in UNIX */
+    end: PropTypes.number.isRequired,
+    /** UTC time zone */
+    utc: PropTypes.number,
+    /** Language */
+    lang: PropTypes.oneOf(['ru', 'en']),
+    /** className */
+    className: PropTypes.string,
+  };
+
+  static defaultProps = {
+    utc: 0,
+    lang: 'ru',
+    className: 'Countdown',
   };
 
   state = {
-    days   : null,
-    hours  : null,
+    days: null,
+    hours: null,
     minutes: null,
-    seconds: null
+    seconds: null,
   };
 
   componentDidMount() {
     this.calculateTime();
-  };
+  }
 
   getCount = (difference, type) => {
-    const result = difference / this.CONSTANTS[ type ];
+    const result = difference / time[type];
 
     if (result >= 1) {
       return Math.floor(result);
@@ -30,30 +43,34 @@ class Countdown extends Component {
 
   calculateTime = () => {
     const { end, utc } = this.props;
-    const now = (new Date().getTime() / 1000) + (utc * this.CONSTANTS.hours);
+    const now = (new Date().getTime() / 1000) + (utc * time.hours);
     let difference = end - now;
 
     const days = this.getCount(difference, 'days');
-    difference = difference - (this.CONSTANTS.days * days);
+    difference -= (time.days * days);
     const hours = this.getCount(difference, 'hours');
-    difference = difference - (this.CONSTANTS.hours * hours);
+    difference -= (time.hours * hours);
     const minutes = this.getCount(difference, 'minutes');
-    const seconds = Math.floor(difference - this.CONSTANTS.minutes * minutes);
+    const seconds = Math.floor(difference - time.minutes * minutes);
 
-    this.setState({ days, hours, minutes, seconds }, () => setTimeout(() => this.calculateTime(end), 1000));
+    this.setState({
+      days, hours, minutes, seconds,
+    },
+    () => setInterval(() => this.calculateTime(end), 1000));
   };
 
   render() {
-    const { className } = this.props;
-    const { days, hours, minutes, seconds } = this.state;
-    const css = className ? className : 'Countdown';
+    const { className, lang } = this.props;
+    const {
+      days, hours, minutes, seconds,
+    } = this.state;
 
     return (
-      <span className={ css }>
-        { `${ !days ? '' : `${ days } дней ` }` }
-        { `${ !hours ? '' : `${ hours } часов ` }` }
-        { `${ !minutes ? '' : `${ minutes } минут ` }` }
-        { `${ !seconds ? '' : `${ seconds } секунд ` }` }
+      <span className={className}>
+        { `${!days ? '' : `${days} ${getEndOfWord(lang, 'days', days)} `}` }
+        { `${!hours ? '' : `${hours} ${getEndOfWord(lang, 'hours', hours)} `}` }
+        { `${!minutes ? '' : `${minutes} ${getEndOfWord(lang, 'minutes', minutes)} `}` }
+        { `${!seconds ? '' : `${seconds} ${getEndOfWord(lang, 'seconds', seconds)} `}` }
       </span>
     );
   }
---
+++ src/helpers.js
@@ -0,0 +1,32 @@
+export const time = {
+  days: 86400,
+  hours: 3600,
+  minutes: 60,
+};
+
+const enEnds = {
+  days: ['day', 'days'],
+  hours: ['hour', 'hours'],
+  minutes: ['minute', 'minutes'],
+  seconds: ['second', 'seconds'],
+};
+
+const ruEnds = {
+  days: ['день', 'дня', 'дней'],
+  hours: ['час', 'часа', 'часов'],
+  minutes: ['минута', 'минуты', 'минут'],
+  seconds: ['секунда', 'секунды', 'секунд'],
+};
+
+const cases = [2, 0, 1, 1, 1, 2];
+
+export const getEndOfWord = (lang, type, count) => {
+  if (lang === 'en') {
+    const index = count === 1 ? 0 : 1;
+    return enEnds[type][index];
+  }
+
+  return ruEnds[type][
+    (count % 100 > 4 && count % 100 < 20) ? 2 : cases[(count % 10 < 5) ? count % 10 : 5]
+  ];
+};
